0bead299fef78f32394135a99feb91c93a2b4a7d
toteabe <toteabe@yahoo.es>
2024-05-20 14:25:22 +0200

    inicial

--- src/test/java/org/iesvdm/sudoku/SudokuTest.java
+++ src/test/java/org/iesvdm/sudoku/SudokuTest.java
@@ -1,7 +1,12 @@
 package org.iesvdm.sudoku;
 
 //import static org.junit.jupiter.api.Assertions.*;
+import static java.time.Duration.ofMinutes;
+import static java.time.Duration.ofSeconds;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.function.Consumer;
@@ -10,13 +15,16 @@ import java.util.function.Function;
 public class SudokuTest {
 
     @Test
-    void failTest() {
+    void fillBoardBasedInCluesRandomlySolvableTest() {
         Sudoku sudoku = new Sudoku();
-        sudoku.fillBoardBasedInCluesRandomlySolvable();
-        //sudoku.fillBoardBasedInCluesRandomly();
+        sudoku.setNumClues(43);
+        assertTimeoutPreemptively(
+                ofSeconds(10), () -> {
+                    sudoku.fillBoardBasedInCluesRandomlySolvable();
+                    //sudoku.fillBoardBasedInCluesRandomly();
+                });
         sudoku.printBoard();
-
-        System.out.println("XXXXXXXXX");
+        System.out.println("XXXXXXXXXSOLUCIÓNXXXXXXXXX");
         sudoku.solveBoard();
         sudoku.printBoard();
     }
@@ -25,8 +33,11 @@ public class SudokuTest {
     void fillBoardBasedInCluesRandomlyTest() {
         Sudoku sudoku = new Sudoku();
         sudoku.fillSolvable((NotPureConsumer<Sudoku>) Sudoku::fillBoardBasedInCluesRandomly);
-        sudoku.printBoard();
-        System.out.println("XXXXXXXXX");
+        assertTimeoutPreemptively(
+                ofSeconds(10), () -> {
+                    sudoku.printBoard();
+                });
+        System.out.println("XXXXXXXXXSOLUCIÓNXXXXXXXXX");
         boolean solved = sudoku.solveBoard();
         System.out.println(solved);
         sudoku.printBoard();
@@ -35,9 +46,13 @@ public class SudokuTest {
     @Test
     void fillBoardBasedInCluesFirstValidTest() {
         Sudoku sudoku = new Sudoku();
-        sudoku.fillSolvable((NotPureConsumer<Sudoku>) Sudoku::fillBoardBasedInCluesFirstValid);
+        sudoku.setNumClues(73);
+        assertTimeoutPreemptively(
+                ofSeconds(10), () -> {
+                    sudoku.fillSolvable((NotPureConsumer<Sudoku>) Sudoku::fillBoardBasedInCluesFirstValid);
+                });
         sudoku.printBoard();
-        System.out.println("XXXXXXXXX");
+        System.out.println("XXXXXXXXXSOLUCIÓNXXXXXXXXX");
         sudoku.solveBoard();
         sudoku.printBoard();
     }
@@ -45,9 +60,13 @@ public class SudokuTest {
     @Test
     void fillBoardBasedInCluesRandomlyValidFromSetsTest() {
         Sudoku sudoku = new Sudoku();
-        sudoku.fillSolvable((Function<Sudoku, Boolean>) Sudoku::fillBoardBasedInCluesRandomlyValidFromSets);
+        sudoku.setNumClues(53);
+        assertTimeoutPreemptively(
+                ofSeconds(10), () -> {
+                    sudoku.fillSolvable((Function<Sudoku, Boolean>) Sudoku::fillBoardBasedInCluesRandomlyValidFromSets);
+                });
         sudoku.printBoard();
-        System.out.println("XXXXXXXXX");
+        System.out.println("XXXXXXXXXSOLUCIÓNXXXXXXXXX");
         sudoku.solveBoard();
         sudoku.printBoard();
     }
@@ -55,9 +74,13 @@ public class SudokuTest {
     @Test
     void fillBoardBasedInClues50Ramdonly50FirstValidTest() {
         Sudoku sudoku = new Sudoku();
-        sudoku.fillSolvable((NotPureConsumer<Sudoku>) Sudoku::fillBoardBasedInClues50Ramdonly50FirstValid);
+        sudoku.setNumClues(53);
+        assertTimeoutPreemptively(
+                ofSeconds(10), () -> {
+                    sudoku.fillSolvable((NotPureConsumer<Sudoku>) Sudoku::fillBoardBasedInClues50Ramdonly50FirstValid);
+                });
         sudoku.printBoard();
-        System.out.println("XXXXXXXXX");
+        System.out.println("XXXXXXXXXSOLUCIÓNXXXXXXXXX");
         sudoku.solveBoard();
         sudoku.printBoard();
     }
